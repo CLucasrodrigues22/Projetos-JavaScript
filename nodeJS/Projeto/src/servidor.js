b5ed0a3d0daf05a09f59c336db3bbcd704682308
CLucasrodrigues22 <c.lucasrodrigues22@gmail.com>
2021-03-29 23:44:15 -0300

    Up para correçao do body-parser

--- nodeJS/Projeto/src/servidor.js
+++ nodeJS/Projeto/src/servidor.js
@@ -3,14 +3,25 @@ const porta = 3003
 const { request } = require('express')
 const express = require('express')
 const app = express()
+const bodyParser = require('body-parser')
+const bancoDeDados = require('./bancoDeDados')
 
-// app.get('/produtos', (req, res, next) => {
-//     console.log('MIddleware 1...')
-//     next()
-// })
+app.use(bodyParser.urlencoded({ extended: true }))
 
 app.get('/produtos', (req, res, next) => {
-    res.send({ nome: 'Notebook', preco: 123.45 }) // Converte para JSON automaticamente
+    res.send(bancoDeDados.getProdutos())
+})
+
+app.get('/produtos/:id', (req, res, next) => {
+    res.send(bancoDeDados.getProduto(req.param.id))
+})
+
+app.post('/produtos', (req, res, next) => {
+    const produto = bancoDeDados.salvarProduto({
+        nome: req.body.name,
+        preco: req.body.preco
+    })
+    res.send(produto) //JSON
 })
 
 app.listen(porta, () => {
